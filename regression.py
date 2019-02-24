16298ae0226492147869e3b43e65198613a0a5c3
anandijain <anandj@uchicago.edu>
2019-02-24 10:23:51 -0600

    regression runs on bovada data

--- regression.py
+++ regression.py
@@ -5,58 +5,74 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-# targets_data =
-df = pd.read_csv('data.csv')
-df = df.dropna()
-xt = df.iloc[:, 5:30]
-yt = df[['game_id']]
-# print(x.values)
-x = torch.tensor(xt.values, dtype=float64)
-y = torch.tensor(yt.values, dtype=float64)
-x.dtype
-y.dtype
-# print(xt)
-# plt.scatter(x, y)
-# plt.show()
+from sklearn.preprocessing import StandardScaler
 
-# x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(100, 1)
-# y = x.pow(2) + 0.2*torch.rand(x.size()) 
+scaler = StandardScaler()
+cols = 0
+headers = ['a_team', 'h_team', 'league', 'game_id',
+                'a_pts', 'h_pts', 'secs', 'status', 'a_win', 'h_win', 'last_mod_to_start',
+                'num_markets', 'a_odds_ml', 'h_odds_ml', 'a_hcap_tot', 'h_hcap_tot']
+
+
+def read_csv(fn, dummie):  # read csv and scale data
+    raw = pd.read_csv(fn + '.csv', usecols=headers)
+    raw = raw.dropna()
+    raw = pd.get_dummies(data=raw, columns=['a_team', 'h_team', 'league'])
+    return raw.copy()
+
+def scale(data):
+    scaled = scaler.fit_transform(data.values)
+    return scaled
+
+df = read_csv('nba2', 1)
+cols = df.columns
+df = scale(df)
+
+def features(df):
+    y_tmp = df[:, :1]
+    x_tmp = df[:, 1:]
+
+    X = torch.FloatTensor(x_tmp)
+    Y = torch.FloatTensor(y_tmp)
+    return X, Y
+
+X, Y = features(df)    
 
 class Net(torch.nn.Module):
-    def __init__(self, n_feature, n_hidden, n_output):
-        super(Net, self).__init__()
-        self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
-        self.predict = torch.nn.Linear(n_hidden, n_output)   # output layer
+    def __init__(self):
+        super().__init__()
+        self.linear = torch.nn.Linear(53, 1)
 
     def forward(self, x):
-        x = F.relu(self.hidden(x))      # activation function for hidden layer
-        x = self.predict(x)             # linear output
-        return x
+        y_pred = self.linear(x)
+        return y_pred
 
-net = Net(n_feature=1, n_hidden=10, n_output=1)     # define the network
-print(net)  # net architecture
+net = Net()
 
-optimizer = torch.optim.SGD(net.parameters(), lr=0.2)
 loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
+optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
 
-plt.ion()   # something about plotting
+def run(train_test, data, labels):  # train == 1, test == else
+    for epoch in range(200):
+        y_pred = net(data)
+        loss = loss_func(y_pred, labels)
 
-for t in range(200):
-    prediction = net(x)     # input x and predict based on x
+        if train_test == 1:  
+            plt.scatter(epoch, loss.item(), color='r', s=10, marker='o')
+        else:
+            plt.scatter(epoch, loss.item(), color='b', s=10, marker='o')
 
-    loss = loss_func(prediction, y)     # must be (1. nn output, 2. target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    optimizer.zero_grad()   # clear gradients for next train
-    loss.backward()         # backpropagation, compute gradients
-    optimizer.step()        # apply gradients
+run(1, X, Y)
 
-    if t % 5 == 0:
-        # plot and show learning process
-        plt.cla()
-        plt.scatter(x.data.numpy(), y.data.numpy())
-        plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
-        plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color':  'red'})
-        plt.pause(0.1)
+test = read_csv('data', 0)
+test = test.reindex(columns=cols, fill_value=0)
+test = scale(test)
+X_test, Y_test = features(test)
 
-plt.ioff()
+run(0, X_test, Y_test)
+
 plt.show()
