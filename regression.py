9f542940c07199c0c99fcdba4fd85feeea0c0e08
AnandIJain <anandj@uchicago.edu>
2019-03-08 13:10:36 -0600

    incorporating pytorch dataloader

--- regression.py
+++ regression.py
@@ -2,25 +2,42 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import StandardScaler
 
-train_fn = 'nba2'
-test_fn = 'college'
-num_cols = 0
+# credit https://github.com/utkuozbulak/pytorch-custom-dataset-examples
 
-scaler = StandardScaler()
-cols = 0
+fn = 'nba2'
 
 headers = ['a_team', 'h_team', 'league', 'game_id',
                 'a_pts', 'h_pts', 'secs', 'status', 'a_win', 'h_win', 'last_mod_to_start',
                 'num_markets', 'a_odds_ml', 'h_odds_ml', 'a_hcap_tot', 'h_hcap_tot']
 
+class Df(Dataset):
+    def __init__(self, df):
+        # self.to_tensor = torch.tensor()
+        self.data_len = len(df.index)
+        self.labels = df['a_odds_ml']
+        print(self.labels)
+        self.data = df.drop(['a_odds_ml'], axis=1)
 
-def read_csv(fn, dummie):  # read csv and scale data
+    def __getitem__(self, index):
+        line = self.data.iloc[index, :]
+        line_tensor = torch.tensor(line)
+        line_label = self.labels[index]
+        label_tensor = torch.tensor(line_label)
+
+        return (line_tensor, label_tensor)
+
+    def __len__(self):
+        return self.data_len
+
+
+def read_csv(fn):  # read csv and scale data
     raw = pd.read_csv(fn + '.csv', usecols=headers)
     raw = raw.dropna()
     raw = pd.get_dummies(data=raw, columns=['a_team', 'h_team', 'league'])
@@ -29,22 +46,15 @@ def read_csv(fn, dummie):  # read csv and scale data
     return raw.copy()
 
 def train_test(df):
-    test = df.sample(frac=0.3,random_state=42)
+    test = df.sample(frac=0.5,random_state=42)
     train = df.drop(test.index)
     return train, test
 
-
 def scale(data):
+    scaler = StandardScaler()
     scaled = scaler.fit_transform(data.values)
     return scaled
-
-df = read_csv(train_fn, 1)
-train, test = train_test(df)
-cols = df.columns
-num_cols = len(cols)
-# df = scale(df)
-# print(df)
-
+ 
 def get_batch(df, col='a_odds_ml', batch_size=1):
     batch = df.sample(batch_size)
     Y = batch[col]
@@ -56,34 +66,30 @@ def get_batch(df, col='a_odds_ml', batch_size=1):
     return X, Y
 
 
-def features(df):
-    y_tmp = df[:, :1]
-    x_tmp = df[:, 1:]
+tmp_df = read_csv(fn)
+train, test = train_test(tmp_df)
+train = Df(train)
+test = Df(test)
 
-    X = torch.FloatTensor(x_tmp)
-    Y = torch.FloatTensor(y_tmp)
-    return X, Y
-
-
-X, Y = get_batch(df)
-num_teams = X.shape[1]
-n_in, n_h, n_out, batch_size = num_teams, num_teams // 2, 128, 128
-
-# class Net(torch.nn.Module):
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.l1 = torch.nn.Linear(n_in, n_h)
-#         self.l2 = torch.nn.Linear(n_h, n_h)
-#         self.l3 = torch.nn.Linear(n_h, n_out)
+# print(ttrain.shape)
 
 
-#     def forward(self, x):
-#         print('f')
-#         x = self.l1(x)
-#         x = self.l2(x)
-#         x = self.l3(x)
-#         print(x)
-#         return x
+num_cols = train.data.shape[1]
+
+input_size = num_cols
+hidden_size = 50
+num_classes = 3
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.01
+
+train_loader = DataLoader(dataset=train, batch_size=batch_size, shuffle=True)
+data, label = enumerate(train_loader)
+print(data)
+print('label')
+print(label)
+
+test_loader = DataLoader(dataset=test, batch_size=batch_size, shuffle=False)
 
 
 class NeuralNet(nn.Module):
@@ -99,41 +105,43 @@ class NeuralNet(nn.Module):
         out = self.fc2(out)
         return out
 
-net = NeuralNet(n_in, n_h, n_out)
+net = NeuralNet(input_size, hidden_size, num_classes)
 
 lr = 1e-4
-criterion = nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-
-
-def step(data, labels, steps): 
-    y_pred = net(data)
-    print('.', end="")
-    loss = criterion(y_pred, labels) 
-    # print('loss: ')
-    print(loss)
-    steps += 1
-    plt.scatter(steps, loss.item(), color='r', s=10, marker='o')
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    
-
 
 EPOCHS = 600
 print(batch_size)
+steps = 0
 for i in range(EPOCHS):
-    X, Y = get_batch(train, batch_size=batch_size)
-    steps = 0
-    step(X, Y, steps)
+    for j, (data, labels) in enumerate(train_loader):
+        y_pred = net(data)
+        loss = criterion(y_pred, labels) 
+        plt.scatter(steps, loss.item(), color='r', s=10, marker='o')
 
-# Plot the graph
-# predicted = net(torch.from_numpy()).detach().numpy()
-# plt.plot(train, y_train, 'ro', label='Original data')
-# plt.plot(x_train, predicted, label='Fitted line')
-# plt.legend()
-# plt.show()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        steps += 1
+
+
+def test():
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for i in range(EPOCHS):
+            for i, (data, labels) in enumerate(train_loader):
+                outputs = net(data)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+
+test()
 
 # Save the model checkpoint
 torch.save(net.state_dict(), 'model.ckpt')
+
+plt.show()
