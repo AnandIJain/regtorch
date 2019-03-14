3ec4dec227bf8d4f4624f416b437ae7b16f5ff26
AnandIJain <anandj@uchicago.edu>
2019-03-14 13:52:42 -0500

    not stable, working on testing set

--- autoencoder.py
+++ autoencoder.py
@@ -1,11 +1,4 @@
-"""
-View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
-My Youtube Channel: https://www.youtube.com/user/MorvanZhou
-"""
-headers = [# 'a_team', 'h_team', 'sport', 'league', 
-                'game_id', 'cur_time',
-                'a_pts', 'h_pts', 'secs', 'status', 'a_win', 'h_win', 'last_mod_to_start',
-                'num_markets', 'a_odds_ml', 'h_odds_ml', 'a_hcap_tot', 'h_hcap_tot']
+
 import torch
 import torch.nn as nn
 import torch.utils.data as Data
@@ -19,22 +12,24 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler
 
-# torch.manual_seed(1)    # reproducible
+"""
+Thanks to https://morvanzhou.github.io/tutorials/
+https://www.youtube.com/user/MorvanZhou
+"""
 
-# Hyper Parameters
-EPOCH = 10
-BATCH_SIZE = 64
-LR = 0.005         # learning rate
-N_TEST_IMG = 5
+
+headers = [# 'a_team', 'h_team', 'sport', 'league', 
+                'game_id', 'cur_time',
+                'a_pts', 'h_pts', 'secs', 'status', 'a_win', 'h_win', 'last_mod_to_start',
+                'num_markets', 'a_odds_ml', 'h_odds_ml', 'a_hcap_tot', 'h_hcap_tot']
+
+
 
 
 class Df(Dataset):
     def __init__(self, np_df, unscaled):
-        # self.to_tensor = torch.tensor()
         self.data_len = len(np_df)
-        # self.data_len = len(np_df.index)
         self.data = np_df
-        # self.t_data = torch.tensor(self.data)
         self.unscaled_data = unscaled
         print(self.data_len)
 
@@ -50,7 +45,7 @@ class Df(Dataset):
         return self.data_len
 
 
-def read_csv(fn='nba2'):  # read csv and scale data
+def read_csv(fn='data/nba2'):  # read csv and scale data
     raw = pd.read_csv(fn + '.csv')
     raw = raw.dropna()
     raw = pd.get_dummies(data=raw, columns=[ 'a_team', 'h_team', 'league', 'sport'])
@@ -60,10 +55,12 @@ def read_csv(fn='nba2'):  # read csv and scale data
     raw = raw.sort_values('cur_time', axis=0)
     return raw.copy()
 
+
 def train_test(df):
     test = df.sample(frac=0.5,random_state=None)
     train = df.drop(test.index)
     return train, test
+
 
 def scale(data):
     scaler = StandardScaler()
@@ -73,6 +70,9 @@ def scale(data):
     # print(scaled.shape)
     return scaled
 
+EPOCH = 1 
+LR = 0.005
+N_TEST_IMG = 5
 
 tmp_df = read_csv()
 train_df, test_df = train_test(tmp_df)
@@ -83,13 +83,11 @@ scaled_test = scale(test_df)
 train = Df(scaled_train, train_df.values)
 test = Df(scaled_test, test_df.values)
 
-
 batch_size = 1
 num_cols = tmp_df.shape[1]
 
 input_size = num_cols
-# output_size = 10
-learning_rate = 0.1
+
 
 train_loader = DataLoader(dataset=train, batch_size=batch_size, shuffle=False)
 test_loader = DataLoader(dataset=test, batch_size=batch_size, shuffle=False)
@@ -157,30 +155,19 @@ for epoch in range(EPOCH):
         cur_data = x.float()
         b_x = prev_data.view(-1, num_cols).float()
         b_y = cur_data.view(-1, num_cols).float()
-        # pred = net(prev_data)
-        # encoded, decoded = autoencoder(b_x)
+
         encoded, decoded = autoencoder(prev_data)
 
         loss = loss_func(decoded, cur_data)
-        with torch.no_grad():
-            if loss >= 2:
-                print('decoded: ')
-                print(decoded)
 
-                print('cur_data: ')
-                print(cur_data)
-
-                print('unscaled: ')
-                print(unscaled)
-                # print(unscaled[0:20])
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        if step % 10 == 0:
+        if step % 100 == 0:
             with torch.no_grad():
                 print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy())
-                view_data = x.view(-1, 90).type(torch.FloatTensor)/255.
+                view_data = prev_data.view(-1, 90).type(torch.FloatTensor)/255.
                 # plotting decoded image (second row)
                 _, decoded_data = autoencoder(view_data)
                 for i in range(N_TEST_IMG):
@@ -197,21 +184,27 @@ plt.ioff()
 plt.show()
 
 
-torch.save(net.state_dict(), 'auto.ckpt')
+torch.save(autoencoder.state_dict(), 'models/auto.ckpt')
 
-# visualize in 3D plot
-view_data = torch.tensor(test.__getitem__).view(-1, 91).type(torch.FloatTensor)/255.
+for epoch in range(EPOCH):
+    for step, (x, unscaled) in enumerate(test_loader):
 
-encoded_data, _ = autoencoder(view_data)
+        prev_data = cur_data
+        cur_data = x.float()
 
-fig = plt.figure(2); ax = Axes3D(fig)
+        b_x = prev_data.view(-1, num_cols).float()
+        b_y = cur_data.view(-1, num_cols).float()
 
-X, Y, Z = encoded_data.data[:, 0].numpy(), encoded_data.data[:, 1].numpy(), encoded_data.data[:, 2].numpy()
-values = train_data.train_labels[:200].numpy()
+        encoded_data, _ = autoencoder(prev_data)
 
-for x, y, z, s in zip(X, Y, Z, values):
-    c = cm.rainbow(int(255*s/9)); ax.text(x, y, z, s, backgroundcolor=c)
+        fig = plt.figure(2); ax = Axes3D(fig)
 
-ax.set_xlim(X.min(), X.max()); ax.set_ylim(Y.min(), Y.max()); ax.set_zlim(Z.min(), Z.max())
-torch.save(net.state_dict(), 'auto.ckpt')
+        X, Y, Z = encoded_data.data[:, 0].numpy(), encoded_data.data[:, 1].numpy(), encoded_data.data[:, 2].numpy()
+        values = test.data.numpy()
+
+        for x, y, z, s in zip(X, Y, Z, values):
+            c = cm.rainbow(int(255*s/9)); ax.text(x, y, z, s, backgroundcolor=c)
+
+        ax.set_xlim(X.min(), X.max()); ax.set_ylim(Y.min(), Y.max()); ax.set_zlim(Z.min(), Z.max())
+torch.save(net.state_dict(), 'models/auto.ckpt')
 plt.show()
